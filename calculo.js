cec6d83fb7b06f66b26da5f517120c11c56acd94
Raphael Miquelis <raphael.miquelis@g.globo>
2021-12-27 14:56:45 -0300

    add order

--- calculo.js
+++ calculo.js
@@ -139,6 +139,7 @@ async function createTable() {
       // language: {
       //   url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Portuguese-Brasil.json",
       // },
+      order: [[0, "desc"]],
       lengthChange: false,
       buttons: ["copy", "excel", "pdf", "colvis"],
     });
